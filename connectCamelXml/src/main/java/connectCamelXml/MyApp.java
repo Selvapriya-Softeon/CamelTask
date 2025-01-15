6b2b039f4cb257afcd5d28f5c9cc8f0bc1a9a861
Selvapriya-Softeon <selvapriyagopalraj31@gmail.com>
2025-01-15 17:50:02 +0530

    Add files via upload

---
+++ connectCamelXml/src/main/java/connectCamelXml/MyApp.java
@@ -0,0 +1,20 @@
+package connectCamelXml;
+
+import org.apache.camel.spring.Main;
+
+public class MyApp {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		Main m = new Main();
+
+		m.setApplicationContextUri("META-INF/spring/camel-context.xml");
+		
+		m.start();
+		Thread.sleep(100000);
+		m.stop();
+}
+
+
+}
